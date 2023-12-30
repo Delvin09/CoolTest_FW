df3d8d84065121cb2e0f50f254df8078de97a320
Olena <pysarenkoelena@gmail.com>
2023-12-30 17:31:46 +0200

    Vers1

--- CoolTest.Core/TestGroup.cs
+++ CoolTest.Core/TestGroup.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Immutable;
+﻿using CoolTest.Abstarctions;
+using System.Collections.Immutable;
+using System.Reflection;
 
 namespace CoolTest.Core
 {
@@ -16,7 +18,44 @@ namespace CoolTest.Core
             {
                 var subject = Activator.CreateInstance(Type);
 
-                test.Run(subject);
+                try
+                {
+                    test.Run(subject);
+                }
+                catch (AssertFailException ex)
+                {
+                    Console.WriteLine($"Error in test {test.Name}: {ex.Message}");
+                    throw new AssertFailException($"Stop the execution of the test group {Name}: {ex.Message}");
+                }
+                finally
+                {
+                    RunClean(subject, test);
+                }
+            }
+        }
+        private void RunClean(object subject, Test test)
+        {
+            var cleanMethods = subject.GetType()
+                .GetMethods()
+                .Where(m => m.Name == "Clean" && m.GetCustomAttribute<TestAttribute>() != null);
+
+            foreach (var cleanMethod in cleanMethods)
+            {
+                try
+                {
+                    cleanMethod.Invoke(subject, null);
+
+                    if (cleanMethod.GetCustomAttribute<TestAttribute>() != null)
+                    {
+                        Console.WriteLine($"The clean method after the test {test.Name} has a test attribute. Stop the execution of the test group.");
+                        throw new AssertFailException($"The clean method after the test {test.Name} has a test attribute. Stop the execution of the test group.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error calling the clean method after a test {test.Name}: {ex.Message}");
+                    throw new AssertFailException($"Error calling the clean method after a test {test.Name}: {ex.Message}");
+                }
             }
         }
     }
