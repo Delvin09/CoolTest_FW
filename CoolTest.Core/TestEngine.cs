ecfe58d8c49900d1d568dfac0d2f221943a11fa1
Olejjas <mollstam4455@gmail.com>
2024-01-03 10:21:42 +0200

    edits have been made, awaiting responses.

--- CoolTest.Core/TestEngine.cs
+++ CoolTest.Core/TestEngine.cs
@@ -1,9 +1,18 @@
-﻿namespace CoolTest.Core
+﻿using System.Xml.Serialization;
+
+namespace CoolTest.Core
 {
-    public class TestEngine
+     public class TestEngine
     {
+        public EventHandler<GroupRunEventArgs>? BeforeGroupRun;
+        public EventHandler<GroupRunEventArgs>? AfterGroupRun;
+        public EventHandler<GroupRunEventArgs>? ForwardedBeforeGroupRun;
+        public EventHandler<GroupRunEventArgs>? ForwardedAfterGroupRun;
         public void Run(string[] assemblies)
         {
+            BeforeGroupRun += OnBeforeGroupRun;
+            AfterGroupRun += OnAfterGroupRun;
+
             foreach (var assembly in assemblies)
             {
                 using (var provider = new TestProvider(assembly))
@@ -11,10 +20,45 @@
                     var testGroups = provider.GetTests();
                     foreach (var group in testGroups)
                     {
+                        OnBeforeGroupRun(this, new GroupRunEventArgs(group));
+                        OnForwardedBeforeGroupRun(new GroupRunEventArgs(group));
+
                         group.Run();
+
+                        OnAfterGroupRun(this, new GroupRunEventArgs(group));
+                        OnForwardedAfterGroupRun(new GroupRunEventArgs(group));
                     }
                 }
             }
+
+            BeforeGroupRun -= OnBeforeGroupRun;
+            AfterGroupRun -= OnAfterGroupRun;
         }
+
+        private void OnBeforeGroupRun(object sender, GroupRunEventArgs e)
+        {
+            BeforeGroupRun?.Invoke(this, e);
+        }
+
+        private void OnAfterGroupRun(object sender, GroupRunEventArgs e)
+        {
+            AfterGroupRun?.Invoke(this, e);
+        }
+
+        private void OnForwardedBeforeGroupRun(GroupRunEventArgs e)
+        {
+            ForwardedBeforeGroupRun?.Invoke(this, e);
+        }
+
+        private void OnForwardedAfterGroupRun(GroupRunEventArgs e)
+        {
+            ForwardedAfterGroupRun?.Invoke(this, e);
+        }
+    }
+    public class GroupRunEventArgs : EventArgs 
+    {
+        internal TestGroup TestGroup { get; }
+
+        internal GroupRunEventArgs(TestGroup testGroup) => TestGroup = testGroup;
     }
 }
--- CoolTest.Core/TestGroup.cs
+++ CoolTest.Core/TestGroup.cs
@@ -9,29 +9,50 @@ namespace CoolTest.Core
         public string Name { get; set; }
         public Type Type { get; set; }
         public ImmutableArray<Test> Tests { get; set; }
+        public DateTime StartTime { get; private set; }
+        public DateTime EndTime { get; private set; }
+        public bool IsRunSuccessful { get; private set; }
+        public string StartedGroupName { get; private set; }
+        public ImmutableArray<string> AllTestNames { get; private set; }
+        public ImmutableArray<string> FailedTestNames { get; private set; }
         public EventHandler BeforeRun;
         public EventHandler AfterRun;
 
         public void Run()
         {
+            StartTime = DateTime.Now;
+            StartedGroupName = Name;
+            AllTestNames = Tests.Select(test => test.Name).ToImmutableArray();
             OnBeforeRun(EventArgs.Empty);
+
+            List<string> failedTests = new List<string>();
 
             foreach (var test in Tests)
             {
                 var subject = Activator.CreateInstance(Type);
-
-                test.Run(subject);
+                try
+                {
+                    test.Run(subject);
+                }
+                catch (Exception)
+                {
+                    failedTests.Add(test.Name);
+                }
             }
 
+            EndTime = DateTime.Now;
             OnAfterRun(EventArgs.Empty);
+
+            FailedTestNames = failedTests.ToImmutableArray();
+            IsRunSuccessful = FailedTestNames.IsEmpty;
         }
 
-        protected virtual void OnBeforeRun(EventArgs e) 
+        protected void OnBeforeRun(EventArgs e)
         {
             BeforeRun?.Invoke(this, e);
         }
 
-        protected virtual void OnAfterRun(EventArgs e) 
+        protected void OnAfterRun(EventArgs e)
         {
             AfterRun?.Invoke(this, e);
         }
