d25ef36558a3746a9f0f85cf917d36a91b4cb12b
Oleksandr Nikiforov <nikiforov.aleksandr@gmail.com>
2024-01-05 15:25:53 -0800

    #13 logging to a file

--- CoolTest.Core/Logger/Logger.cs
+++ CoolTest.Core/Logger/Logger.cs
@@ -8,7 +8,8 @@ namespace CoolTest.Core.Logger
 {
     public class Logger : ILogger
     {
-        private const string dateFormat = "yyyy-MM-dd HH:mm:ss";
+        private const string logLineDateTimeFormat = "yyyy-MM-dd HH:mm:ss";
+        private const string fileDateTimeFormat = "yyyy-MM-dd HHmmss";
         public enum MessageLevel
         {
             ERROR = 1,
@@ -16,16 +17,43 @@ namespace CoolTest.Core.Logger
             INFO,
         }
 
+        private FileStream _stream;
+        private MessageLevel _messageLoggingLevel;
+        public Logger(MessageLevel messageLoggingLevel = MessageLevel.INFO)
+        {
+            var logPath = Environment.CurrentDirectory;
+
+            _messageLoggingLevel = messageLoggingLevel;
+
+            _stream = File.OpenWrite(Path.Combine(logPath, $"log_{DateTime.Now.ToString(fileDateTimeFormat)}.txt"));
+        }
+
+        public void Dispose() { _stream.Dispose(); }
+
         private void logMessageLevel (string msg, MessageLevel level)
         {
-            string log = String.Join(" | ", DateTime.Now.ToString(dateFormat), $"[{level}]", msg);
-            Console.WriteLine(log);
+            if (_messageLoggingLevel <= level)
+            {
+                string log = String.Join(" | ", DateTime.Now.ToString(logLineDateTimeFormat), $"[{level}]", msg) + Environment.NewLine;
+                Console.Write(log);
+                if(_stream != null && _stream.CanWrite)
+                {
+                    _stream.Write(Encoding.UTF8.GetBytes(log));
+                }
+            }
         }
 
         private void logExceptionLevel(Exception ex, MessageLevel level)
         {
-            logMessageLevel(ex.Message, level);
-            Console.WriteLine(ex.StackTrace);
+            if (_messageLoggingLevel <= level)
+            {
+                logMessageLevel(ex.Message, level);
+                Console.WriteLine(ex.StackTrace);
+                if (_stream != null && _stream.CanWrite && ex.StackTrace != null)
+                {
+                    _stream.Write(Encoding.UTF8.GetBytes(ex.StackTrace));
+                }
+            }
         }
 
         public void LogInfo(string message)
@@ -37,6 +65,7 @@ namespace CoolTest.Core.Logger
         {
             logMessageLevel(message, MessageLevel.WARNING);
         }
+
         public void LogError(Exception ex)
         {
             logExceptionLevel(ex, MessageLevel.ERROR);
